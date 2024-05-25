385f258b05a05d71a7b0cb52f47f26ddb8912db1
Kudsun Khamjamnun <kudsun.k@gmail.com>
2024-05-26 00:42:30 +0700

    add limit 10 mb

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
   const config = new DocumentBuilder()
     .setTitle('NestJS API For Database Connect')
     .setDescription('Handle every thing for mongodb database that collect data of account and project')
